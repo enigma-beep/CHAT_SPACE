b30c5522531fb7fa79510f3af37fa01099c2cd2d
Abhigyan Baruah <55652687+enigma-beep@users.noreply.github.com>
2019-11-03 13:39:21 +0530

    ui update

--- app/src/main/java/com/example/mychatbox/QueryChat.java
+++ app/src/main/java/com/example/mychatbox/QueryChat.java
@@ -86,10 +86,10 @@ public class QueryChat extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_query_chat);
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+        /*if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
             Window w = getWindow();
             w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
-        }
+        }*/
 
         layout = (LinearLayout) findViewById(R.id.layout1);
         layout_2 = (RelativeLayout)findViewById(R.id.layout2);
